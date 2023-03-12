ed9ea4f42c95b4d5f52fc0dc138fed42a0caa614
coderboyakash <akash.chowdhury@weavers-web.com>
2023-03-12 16:30:18 +0530

    login page work

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -12,7 +12,9 @@ const Login: FC = () => {
                 sm={6}
                 className={styles.login_left_section} 
             >
-                <img src="/assets/images/screenshot2.png" />
+                <div className={styles.login_poster}>
+                    <img src="/assets/images/screenshot2.png" />
+                </div>
             </Col>
             <Col 
                 lg={6} 
@@ -20,43 +22,45 @@ const Login: FC = () => {
                 sm={6}
                 className={styles.login_right_section} 
             >
-                <div className={styles.login_form}>
-                    <h2>CodeGram!!</h2>
-                    <form action="">
-                        <Form.Group className={styles.form_group}>
-                            <label htmlFor="email">Phone Number, username or email</label>
-                            <input type="text" id="email" className="form-control" />
-                        </Form.Group>
-                        <Form.Group className={styles.form_group}>
-                            <label htmlFor="">Password</label>
-                            <input type="password" className="form-control" />
-                        </Form.Group>
-                        <Form.Group className={styles.submit_btn}>
-                            <Button>
-                                Log in
-                            </Button>
-                        </Form.Group>
-                        <div className={styles.divider}>
-                            <span>OR</span>
-                        </div>
-                        <div className={styles.login_with_facebook}>
-                            <AiFillFacebook />
-                            &nbsp;Login with facebook
-                        </div>
-                        <div className={styles.forgot_password}>
-                            <a href="">
-                                Forgot Password?
-                            </a>
-                        </div>
+                <div>
+                    <div className={styles.login_form}>
+                        <h2>CodeGram!!</h2>
+                        <form action="">
+                            <Form.Group className={styles.form_group}>
+                                <label htmlFor="email">Phone Number, username or email</label>
+                                <input type="text" id="email" className="form-control" />
+                            </Form.Group>
+                            <Form.Group className={styles.form_group}>
+                                <label htmlFor="">Password</label>
+                                <input type="password" className="form-control" />
+                            </Form.Group>
+                            <Form.Group className={styles.submit_btn}>
+                                <Button>
+                                    Log in
+                                </Button>
+                            </Form.Group>
+                            <div className={styles.divider}>
+                                <span>OR</span>
+                            </div>
+                            <div className={styles.login_with_facebook}>
+                                <AiFillFacebook />
+                                &nbsp;Login with facebook
+                            </div>
+                            <div className={styles.forgot_password}>
+                                <a href="">
+                                    Forgot Password?
+                                </a>
+                            </div>
 
-                    </form>
-                </div>
-                <div className={styles.sign_up_section}>
-                    Don't have an account?
-                    <strong>
-                        &nbsp;
-                        <a href="">Sign up</a>
-                    </strong>
+                        </form>
+                    </div>
+                    <div className={styles.sign_up_section}>
+                        Don't have an account?
+                        <strong>
+                            &nbsp;
+                            <a href="">Sign up</a>
+                        </strong>
+                    </div>
                 </div>
             </Col>
         </Row>
